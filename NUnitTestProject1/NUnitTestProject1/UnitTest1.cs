ac0ed8788a6138b74e6eb31f4cb497b037d9c537
omar-feriaC <36746365+omar-feriaC@users.noreply.github.com>
2019-10-28 13:20:16 -0600

    commit

--- NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -1,16 +1,37 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace Tests
 {
     public class Tests
     {
+        private IWebDriver driver;
+
         [SetUp]
         public void Setup()
         {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Url = "https://www.cypress.io/";
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+            driver.Quit();
         }
 
         [Test]
         public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
         {
             Assert.Pass();
         }
